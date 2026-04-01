21069e3a92c0ec623fac03d4bde5eeda861c8b80
jasdun5511 <152825133+jasdun5511@users.noreply.github.com>
2026-04-01 16:22:54 -0300

    更新 script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-// 获取页面中的音频元素
-const bgm = document.getElementById('gameBgm');
 
-// 监听玩家的第一次点击行为
-document.body.addEventListener('click', function() {
-    // 如果音乐当前是暂停状态（或者还没开始播放）
-    if (bgm.paused) {
-        bgm.play(); // 开始播放
-        console.log("Música tocando! (音乐播放中!)");
-    }
-}, { once: true }); // { once: true } 非常重要！它确保这个检测只触发一次，避免每次点击都重新触发播放命令
 function startGame() {
     alert('初始化脚本... 开始【新游戏】！');
     // 在这里写进入游戏第一幕的代码，例如隐藏菜单层，显示对话框层
@@ -38,3 +28,14 @@ function exitGame() {
         // window.close(); // 仅在独立窗口或套壳应用(如Electron)中有效
     }
 }
+// 获取页面中的音频元素
+const bgm = document.getElementById('gameBgm');
+
+// 监听玩家的第一次点击行为
+document.body.addEventListener('click', function() {
+    // 如果音乐当前是暂停状态（或者还没开始播放）
+    if (bgm.paused) {
+        bgm.play(); // 开始播放
+        console.log("Música tocando! (音乐播放中!)");
+    }
+}, { once: true }); // { once: true } 非常重要！它确保这个检测只触发一次，避免每次点击都重新触发播放命令
